00d82d8af70c2881ba9e0867452d4b60608bd780
Chuck Greenman <charlesgreenman@gmail.com>
2019-06-04 12:30:02 -0400

    Install devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'front#index'
+  get 'front/index'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
