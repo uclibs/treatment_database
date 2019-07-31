e43c1bf5ca5e43b939428259a1f2e60589d70476
Chuck Greenman <charlesgreenman@gmail.com>
2019-07-31 16:20:09 -0400

    Add tests and rebase on qa.

---
+++ spec/views/shared/_navigation.html.erb_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'shared/_navigation.html.erb', type: :view do
+  include Devise::TestHelpers
+
+  before do
+    create(:user)
+    render
+  end
+
+  it 'has a menu with the expected links when signed out' do
+    expect(rendered).to have_link('Conservation Records')
+    expect(rendered).to have_link('Vocabularies')
+    expect(rendered).to have_link('Log in')
+    expect(rendered).to have_link('Sign up')
+  end
+end
