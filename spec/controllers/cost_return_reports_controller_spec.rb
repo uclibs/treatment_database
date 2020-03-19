06135da4b903ebb9a90f609e152c53a8ae176e32
Chuck Greenman <charlesgreenman@gmail.com>
2020-03-19 12:15:39 -0400

    Add number precision for the cost fields.

--- spec/controllers/cost_return_reports_controller_spec.rb
+++ spec/controllers/cost_return_reports_controller_spec.rb
@@ -16,9 +16,9 @@ RSpec.describe CostReturnReportsController, type: :controller do
       shipping_cost: 100.10,
       repair_estimate: 110.10,
       repair_cost: 120.10,
-      invoice_sent_to_business_office: Time.now,
+      invoice_sent_to_business_office: Time.now.utc,
       complete: true,
-      returned_to_origin: Time.now,
+      returned_to_origin: Time.now.utc,
       note: 'This is the note.'
     }
   end
