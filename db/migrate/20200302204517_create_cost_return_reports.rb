c8a77e8f5918eb9d3e5812e41bce311e32752fbc
Sameera Appana <appanasa@Admins-MacBook-Pro.local>
2020-06-30 20:11:24 -0400

    run rubocop

--- db/migrate/20200302204517_create_cost_return_reports.rb
+++ db/migrate/20200302204517_create_cost_return_reports.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class CreateCostReturnReports < ActiveRecord::Migration[5.2]
   def change
     create_table :cost_return_reports do |t|
       # Description Fields
-      t.decimal :shipping_cost, :precision => 8, :scale => 2
-      t.decimal :repair_estimate, :precision => 8, :scale => 2
-      t.decimal :repair_cost, :precision => 8, :scale => 2
+      t.decimal :shipping_cost, precision: 8, scale: 2
+      t.decimal :repair_estimate, precision: 8, scale: 2
+      t.decimal :repair_cost, precision: 8, scale: 2
       t.datetime :invoice_sent_to_business_office
       t.boolean :complete
       t.datetime :returned_to_origin
