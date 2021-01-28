5deb994b50148d7c8796fec4cadbce4c50022e32
harithavytla <haritha.vytla@gmail.com>
2021-01-28 14:54:31 -0500

    Increase test coverage

--- spec/controllers/treatment_reports_controller_spec.rb
+++ spec/controllers/treatment_reports_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe TreatmentReportsController, type: :controller do
     user = create(:user, role: 'admin')
     sign_in(user)
   end
-
+  let(:conservation_record) { create(:conservation_record) }
   let(:valid_attributes) do
     housing_a = create(:controlled_vocabulary, vocabulary: 'housing', key: 'housing')
     {
@@ -39,7 +39,8 @@ RSpec.describe TreatmentReportsController, type: :controller do
       treatment_proposal_housing_narrative: 'Treatment Propsal Housing Narrative',
       treatment_proposal_storage_and_handling_notes: 'Treatment Proposal Storage Handling and Notes',
       treatment_proposal_total_treatment_time: 30,
-      conservators_report: "Conservator's Note"
+      conservators_note: "Conservator's Note",
+      conservation_record_id: conservation_record.id
     }
   end
 
@@ -49,6 +50,48 @@ RSpec.describe TreatmentReportsController, type: :controller do
         conservation_record = create(:conservation_record)
         post :create, params: { conservation_record_id: conservation_record.id, treatment_report: valid_attributes }
         expect(response).to redirect_to(conservation_record_path(conservation_record))
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    context 'with valid params' do
+      let(:new_attributes) do
+        housing_a = create(:controlled_vocabulary, vocabulary: 'housing', key: 'housing')
+        {
+          description_general_remarks: 'General Description Remarks',
+          description_binding: 'Description Binding',
+          description_textblock: 'Description Textblock',
+          description_primary_support: 'Description Support',
+          description_medium: 'Description Medium',
+          description_attachments_inserts: 'Description Attachments Inserts',
+          description_housing: 'Description Housing',
+          condition_summary: 'Condition Summary',
+          condition_binding: 'Condition Binding',
+          condition_textblock: 'Condition Textblock',
+          condition_primary_support: 'Condition Primary Support',
+          condition_medium: 'Condition Medium',
+          condition_housing_id: housing_a.id,
+          condition_housing_narrative: 'Condition Housing Narrative',
+          condition_attachments_inserts: 'Condition Attachments Inserts',
+          condition_previous_treatment: 'Condition Previous Treatment',
+          condition_materials_analysis: 'Condition Materials Analysis',
+          treatment_proposal_proposal: 'Treatment Proposal Proposal',
+          treatment_proposal_housing_need_id: housing_a.id,
+          treatment_proposal_factors_influencing_treatment: 'Treatment Propsal Factors Influencing Treatment',
+          treatment_proposal_performed_treatment: 'Treatment Propsal Performed Treatment',
+          treatment_proposal_housing_provided_id: housing_a.id,
+          treatment_proposal_housing_narrative: 'Treatment Propsal Housing Narrative',
+          treatment_proposal_storage_and_handling_notes: 'Treatment Proposal Storage Handling and Notes',
+          treatment_proposal_total_treatment_time: 30,
+          conservators_note: "Conservator's Note"
+        }
+      end
+      it 'updates the Treatment Report' do
+        treatment_report = TreatmentReport.create! valid_attributes
+        put :update, params: { conservation_record_id: conservation_record.id, id: treatment_report.to_param, treatment_report: new_attributes }
+        treatment_report.reload
+        expect(treatment_report.description_primary_support).to eq('Description Support')
       end
     end
   end
--- spec/helpers/abbreviated_treatment_report_helper_spec.rb
+++ spec/helpers/abbreviated_treatment_report_helper_spec.rb
@@ -13,4 +13,73 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe AbbreviatedTreatmentReportHelper, type: :helper do
+  let(:conservation_record) { create(:conservation_record) }
+  let(:repair_type) { create(:controlled_vocabulary, vocabulary: 'repair_type', key: 'Repair/restore binding') }
+  let(:user) { create(:user, display_name: 'Haritha Vytla') }
+  let(:repair_record) do
+    create(:in_house_repair_record,
+           performed_by_user_id: user.id,
+           repair_type: repair_type.id,
+           conservation_record: conservation_record,
+           minutes_spent: 2)
+  end
+
+  describe 'generate_abbreviated_treatment_report_type' do
+    it 'returns "Repair Type"' do
+      return_value = helper.generate_abbreviated_treatment_report_type(repair_record, 1)
+      expect(return_value).to eq('Repair/restore binding')
+    end
+  end
+
+  describe 'generate_abbreviated_treatment_report_performed_by' do
+    it 'returns the "User ID"' do
+      return_value = helper.generate_abbreviated_treatment_report_performed_by(repair_record, 1)
+      expect(return_value).to eq('Haritha Vytla')
+    end
+  end
+
+  describe 'generate_abbreviated_treatment_report_time' do
+    it 'returns "minutes spent on In House Repair Record"' do
+      return_value = helper.generate_abbreviated_treatment_report_time(repair_record, 1)
+      expect(return_value).to eq('2')
+    end
+  end
+
+  describe 'conservators_note' do
+    let!(:treatment_report) do
+      TreatmentReport.create(conservation_record_id: 1,
+                             description_general_remarks: 'Good',
+                             description_binding: nil,
+                             description_textblock: nil,
+                             description_primary_support: nil,
+                             description_medium: nil,
+                             description_attachments_inserts: nil,
+                             description_housing: nil,
+                             condition_summary: nil,
+                             condition_binding: nil,
+                             condition_textblock: nil,
+                             condition_primary_support: nil,
+                             condition_medium: nil,
+                             condition_housing_id: nil,
+                             condition_housing_narrative: nil,
+                             condition_attachments_inserts: nil,
+                             condition_previous_treatment: nil,
+                             condition_materials_analysis: nil,
+                             treatment_proposal_proposal: nil,
+                             treatment_proposal_housing_need_id: nil,
+                             treatment_proposal_factors_influencing_treatment: nil,
+                             treatment_proposal_performed_treatment: nil,
+                             treatment_proposal_housing_provided_id: nil,
+                             treatment_proposal_housing_narrative: nil,
+                             treatment_proposal_storage_and_handling_notes: nil,
+                             treatment_proposal_total_treatment_time: nil,
+                             created_at: '2021-01-21 19:29:04',
+                             updated_at: '2021-01-21 19:29:04',
+                             conservators_note: nil)
+    end
+    it 'returns conservators note left on In House Repair Record' do
+      return_value = helper.conservators_note(treatment_report, 1)
+      expect(return_value).to eq(nil)
+    end
+  end
 end
