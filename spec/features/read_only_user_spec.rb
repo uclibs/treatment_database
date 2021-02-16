eef50230171d20e8baf8f516aabb454538bd497c
harithavytla <haritha.vytla@gmail.com>
2021-02-16 10:22:51 -0500

    Integration Tests
    Integration Tests for Admin_User
    
    Integration Tests

---
+++ spec/features/read_only_user_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Read Only User Tests', type: :feature do
+  let(:user) { create(:user, role: 'read_only') }
+  let(:conservation_record) { create(:conservation_record, title: 'Farewell to Arms', department: 'ARB Library') }
+  it 'allows User to login and show Conservation Records' do
+    # Login
+
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'notapassword'
+    click_button 'Log in'
+    expect(page).to have_content('Signed in successfully')
+    expect(page).to have_link('Conservation Records')
+
+    # Show Conservation Records
+
+    click_on 'Conservation Records'
+    expect(page).to have_content('Conservation Records')
+    expect(page).to have_no_link('Add Conservation Record')
+    expect(page).to have_no_link('Destroy')
+    expect(page).to have_no_link('Edit', text: 'Edit', exact_text: true)
+    expect(page).to have_link('Show')
+    click_link('Show', match: :prefer_exact)
+    expect(page).to have_content(conservation_record.title)
+    expect(page).to have_no_link('Edit Conservation Record')
+    expect(page).to have_no_button('Add In-House Repairs')
+    expect(page).to have_no_button('Add External Repair')
+    expect(page).to have_no_button('Add Conservators and Technicians')
+    expect(page).to have_button('Save Treatment Report', disabled: false)
+  end
+end
