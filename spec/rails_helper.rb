9b2effeae2f0ab275093cab40f470f59b3071fdc
Sean Crowe <crowesn@ucmail.uc.edu>
2021-07-21 15:23:05 -0400

    resolve failing specs (#151)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,4 +66,5 @@ RSpec.configure do |config|
   Rails.application.load_seed
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
