41d2a89ad747d9a4cd0df18532b1db790abcd6d4
rishitejar <rangarra@mail.uc.edu>
2021-10-04 16:20:34 -0400

    rubocop.yml updated

--- app/helpers/activity_helper.rb
+++ app/helpers/activity_helper.rb
@@ -42,13 +42,13 @@ module ActivityHelper
   def name_to_summary(version)
     case version.item_type
     when 'ConservationRecord'
-      if ConservationRecord.where(id: version.item_id).exists?
+      if ConservationRecord.exists?(id: version.item_id)
         link_to ConservationRecord.find(version.item_id).title, conservation_record_path(version.item_id)
       else
         version.object.nil? ? ' ' : version.object.try(:split, 'title: ').last.try(:split, 'author: ').first.chomp
       end
     when 'TreatmentReport'
-      if TreatmentReport.where(id: version.item_id).exists?
+      if TreatmentReport.exists?(id: version.item_id)
         treatment_report_id = TreatmentReport.find(version.item_id).conservation_record.id
         link_to ConservationRecord.find(treatment_report_id).title.to_s, conservation_record_path(treatment_report_id)
       else
@@ -58,21 +58,21 @@ module ActivityHelper
     when 'User'
       User.find(version.item_id).display_name
     when 'InHouseRepairRecord'
-      if InHouseRepairRecord.where(id: version.item_id).exists?
+      if InHouseRepairRecord.exists?(id: version.item_id)
         in_house_repair_id = InHouseRepairRecord.find(version.item_id).conservation_record.id
         link_to InHouseRepairRecord.find(version.item_id).conservation_record.title, conservation_record_path(in_house_repair_id)
       else
         'Record has been deleted'
       end
     when 'ExternalRepairRecord'
-      if ExternalRepairRecord.where(id: version.item_id).exists?
+      if ExternalRepairRecord.exists?(id: version.item_id)
         external_repair_id = ExternalRepairRecord.find(version.item_id).conservation_record.id
         link_to ExternalRepairRecord.find(version.item_id).conservation_record.title, conservation_record_path(external_repair_id)
       else
         'Record has been deleted'
       end
     when 'CostReturnReport'
-      if CostReturnReport.where(id: version.item_id).exists?
+      if CostReturnReport.exists?(id: version.item_id)
         cost_return_report_id = CostReturnReport.find(version.item_id).conservation_record.id
         link_to ConservationRecord.find(cost_return_report_id).title.to_s, conservation_record_path(cost_return_report_id)
       else
