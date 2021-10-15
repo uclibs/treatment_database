ced40f8b1a065df75b7cd4ece3d0e9f0c5700008
crowesn <crowesn@gmail.com>
2021-10-15 10:22:07 -0400

    test devise-registraitons controller

---
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Users::RegistrationsController, type: :controller do
+  let(:user) { create(:user, role: 'admin') }
+
+  describe 'PUT update/:id' do
+    let(:attr) do
+      { email: 'newemail@example.com',
+        id: user.id,
+        display_name: 'New Display Name',
+        role: 'standard_user',
+        current_password: user.password,
+        account_active: true }
+    end
+
+    before do
+      sign_in user
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      put :update, params: { user: attr }
+      user.reload
+    end
+
+    it { expect(response).to redirect_to(root_path) }
+    it { expect(flash[:notice]).to eq('Your account has been updated successfully.') }
+    it { expect(user.display_name).to eql attr[:display_name] }
+    it { expect(user.email).to eql attr[:email] }
+    it { expect(user.role).to eql attr[:role] }
+    it { expect(user.account_active).to be attr[:account_active] }
+  end
+end
