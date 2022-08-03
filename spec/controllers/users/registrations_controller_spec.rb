3ac09df9b9bbc80d168baeb79d1515cff2d7f5a9
Sean Crowe <1069588+crowesn@users.noreply.github.com>
2022-08-03 14:18:18 -0400

    enable user self edit (#330)
    Co-authored-by: crowesn <crowesn@gmail.com>

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -18,10 +18,25 @@ RSpec.describe Users::RegistrationsController, type: :controller do
     before do
       sign_in user
       @request.env['devise.mapping'] = Devise.mappings[:user]
+      put :update, params: { user: attr }
+      user.reload
     end
 
-    it 'raises an error' do
-      expect { put :update, params: { user: attr } }.to raise_error ActionController::UrlGenerationError
+    it { expect(response).to redirect_to(root_path) }
+    it { expect(flash[:notice]).to eq('Your account has been updated successfully.') }
+    it { expect(user.display_name).to eql attr[:display_name] }
+    it { expect(user.email).to eql attr[:email] }
+    it { expect(user.role).to eql attr[:role] }
+    it { expect(user.account_active).to be attr[:account_active] }
+  end
+
+  describe 'sign up' do
+    before do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      get :new
     end
+
+    it { expect(response).to redirect_to(new_user_session_path) }
+    it { expect(flash[:notice]).to eq('Contact admin to request account') }
   end
 end
