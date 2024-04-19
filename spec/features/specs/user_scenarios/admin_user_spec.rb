86977ed7635bb38794a952d0b7f904e8fadaef31
Janell Huyck <Janell.Huyck@outlook.com>
2024-04-19 15:41:20 -0400

    Log in use specs with context

--- spec/features/specs/user_scenarios/admin_user_spec.rb
+++ spec/features/specs/user_scenarios/admin_user_spec.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
-# admin_user_spec.rb is dedicated to testing the functionalities
-# and permissions unique to administrative users within the application.
-# This spec file assesses the ability of admin users to access and
-# interact with administrative-specific features, such as user and settings
-# management. It ensures that all administrative actions are secured and
-# function correctly, reflecting the critical role of admins in maintaining
-# the system. It checks that these users can perform their tasks without
-# encountering unauthorized access errors or other barriers.
+
+require 'rails_helper'
+
+RSpec.describe "Admin User Functionalities", type: :feature do
+  include_context "admin user context"
+
+  it "passes a test" do
+    expect(1).to eq(1)
+  end
+
+end
