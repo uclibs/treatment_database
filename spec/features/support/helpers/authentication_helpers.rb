03e44989902e4ee3433012c78098e479db0b8ca2
Janell Huyck <Janell.Huyck@outlook.com>
2024-04-19 15:41:20 -0400

    Log in use specs with context

--- spec/features/support/helpers/authentication_helpers.rb
+++ spec/features/support/helpers/authentication_helpers.rb
@@ -3,4 +3,18 @@
 require 'rails_helper'
 
 module AuthenticationHelpers
+  def log_in_as_user(user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'notapassword' # from the factory
+    click_button 'Log in'
+
+    aggregate_failures "checking login and dashboard access" do
+      expect(page).to have_content('Signed in successfully')
+      expect(page).to have_content('Conservation Records')
+      expect(page).to_not have_content('Invalid Email or password.')
+      expect(page).to_not have_content('Log in')
+      expect(page).to_not have_content('You are already signed in.')
+    end
+  end
 end
