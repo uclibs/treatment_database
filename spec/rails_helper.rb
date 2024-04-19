9a7bfe02340db4deeaeb832fdb1d98daa6716ffe
Janell Huyck <Janell.Huyck@outlook.com>
2024-04-19 14:47:56 -0400

    Restore spec helper and rails helper to original versions

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,19 +12,19 @@ require 'paper_trail/frameworks/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/shared_contexts/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
-# run as spec files by default. This means that files in spec/shared_contexts that end
+# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# run as spec files by default. This means that files in spec/support that end
 # in _spec.rb will both be required and run as specs, causing the specs to be
 # run twice. It is recommended that you do not name files matching this glob to
 # end with _spec.rb. You can configure this pattern with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 #
 # The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the shared_contexts
+# of increasing the boot-up time by auto-requiring all files in the support
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the shared_contexts files necessary.
+# require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'shared_contexts', '**', '*.rb')].each { |f| require f }
+# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
