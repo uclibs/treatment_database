c9f55adfc9862736a1ae67348f109af10d5aaab8
Janell Huyck <Janell.Huyck@outlook.com>
2024-04-19 16:05:14 -0400

    Rubocop:

--- spec/features/specs/user_scenarios/admin_user_spec.rb
+++ spec/features/specs/user_scenarios/admin_user_spec.rb
@@ -2,11 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe "Admin User Functionalities", type: :feature do
-  include_context "admin user context"
+RSpec.describe 'Admin User Functionalities', type: :feature do
+  include_context 'admin user context'
 
-  it "passes a test" do
+  it 'passes a test' do
     expect(1).to eq(1)
   end
-
 end
--- spec/features/support/helpers/authentication_helpers.rb
+++ spec/features/support/helpers/authentication_helpers.rb
@@ -8,13 +8,24 @@ module AuthenticationHelpers
     fill_in 'Email', with: user.email
     fill_in 'Password', with: 'notapassword' # from the factory
     click_button 'Log in'
+    validate_login_success
+  end
 
-    aggregate_failures "checking login and dashboard access" do
-      expect(page).to have_content('Signed in successfully')
-      expect(page).to have_content('Conservation Records')
-      expect(page).to_not have_content('Invalid Email or password.')
-      expect(page).to_not have_content('Log in')
-      expect(page).to_not have_content('You are already signed in.')
-    end
+  private
+
+  def validate_login_success
+    check_successful_sign_in
+    check_no_error_messages
+  end
+
+  def check_successful_sign_in
+    expect(page).to have_content('Signed in successfully')
+    expect(page).to have_content('Conservation Records')
+  end
+
+  def check_no_error_messages
+    expect(page).to_not have_content('Invalid Email or password.')
+    expect(page).to_not have_content('Log in')
+    expect(page).to_not have_content('You are already signed in.')
   end
 end
