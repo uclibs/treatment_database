521e4a902437a5da542e832eaa008aba1c324aa5
Janell-Huyck <huyckjl@ucmail.uc.edu>
2024-04-23 13:28:29 -0400

    Rebase merge conflict

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -100,7 +100,7 @@ RSpec.configure do |config|
   #   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
   #   config.filter_run_when_matching :focus
   #
-  #   # Allows RSpec to persist some state between runs in order to shared_contexts
+  #   # Allows RSpec to persist some state between runs in order to support
   #   # the `--only-failures` and `--next-failure` CLI options. We recommend
   #   # you configure your source control system to ignore this file.
   #   config.example_status_persistence_file_path = "spec/examples.txt"
