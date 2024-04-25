77bd1671880b85bfea68a4722706c65bea0755db
Janell-Huyck <huyckjl@ucmail.uc.edu>
2024-04-25 13:37:24 -0400

    Check headers and search functionality

--- spec/features/specs/user_scenarios/admin_user_spec.rb
+++ spec/features/specs/user_scenarios/admin_user_spec.rb
@@ -5,6 +5,8 @@ require 'rails_helper'
 RSpec.describe 'Admin User Functionalities', type: :feature do
   include_context 'admin user context'
 
+  it_behaves_like 'has an admin user header'
+
   it_behaves_like 'index page access for authenticated users'
   it_behaves_like 'view conservation record details'
   it_behaves_like 'view controlled vocabularies'
