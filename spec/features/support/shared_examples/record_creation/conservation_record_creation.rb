550914c4c8884e5eb9d8f875a30a34e77bf4b890
Janell Huyck <janell.huyck@outlook.com>
2024-04-30 14:12:42 -0400

    MERGE TO FEATURE BRANCH - 214 Test Conservation Record Creation - MERGE FIRST (#485)
    * Lay out proposed file structure for tests
    
    * Update rails_helper.rb and fix broken tests
    
    * Merge conflicts
    
    * Rebase merge conflict
    
    * Log in use specs with context
    
    * Rubocop:
    
    * Rubocop
    
    * Use shared examples and context for page viewing
    
    * Check headers and search functionality
    
    * Lay out proposed file structure for tests
    
    * Merge conflict resolved
    
    * Merge conflicts
    
    * Rebase merge conflict
    
    * Rubocop:
    
    * Test conservation_record creation in feature tests
    
    * Fix accidental rubocop error created by manual merge conflict resolution

---
+++ spec/features/support/shared_examples/record_creation/conservation_record_creation.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+# spec/support/shared_examples/conservation_records.rb
+
+require 'rails_helper'
+
+RSpec.shared_examples 'creates new conservation records' do
+  let(:conservation_record) { build(:conservation_record) }
+
+  it 'creates a new conservation record' do
+    click_on 'New Conservation Record'
+    expect(page).to have_selector('h1', text: 'New Conservation Record')
+
+    fill_in 'Date received in preservation services', with: '04/15/2024'
+    select('PLCH', from: 'Department', match: :first)
+    fill_in 'Title', with: conservation_record.title
+    fill_in 'Author', with: conservation_record.author
+    fill_in 'Imprint', with: conservation_record.imprint
+    fill_in 'Call number', with: conservation_record.call_number
+    fill_in 'Item record number', with: conservation_record.item_record_number
+
+    click_on 'Create Conservation record'
+
+    expect(flash_notice).to have_content('Conservation record was successfully created')
+    expect(page).to have_selector('h1', text: conservation_record.title)
+    expect(page).to have_link('Edit Conservation Record')
+  end
+end
