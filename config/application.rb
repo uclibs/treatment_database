21bac7524ea7d1e2025d08faa7bc0750c307549c
Janell Huyck <Janell.Huyck@outlook.com>
2024-04-30 13:31:26 -0400

    Add SSO with Shibboleth

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require_relative 'boot'
 
 require 'rails/all'
 require 'dotenv-rails'
+require_relative '../lib/middleware/shibboleth'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,6 +16,9 @@ module TreatmentDatabase
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    # Load middleware for Shibboleth
+    config.middleware.use Middleware::Shibboleth
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
