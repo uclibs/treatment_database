7f5346b16b3dddab3249eb4389f9c62e233c3a74
Janell Huyck <Janell.Huyck@outlook.com>
2024-04-30 13:31:26 -0400

    Copy ucrate tests - first pass

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,3 +9,19 @@ FactoryBot.define do
     account_active { true }
   end
 end
+
+# The following is from ucrate's user factory:
+#  factory :shibboleth_user, class: 'User' do
+#       transient do
+#         count { 1 }
+#         person_pid { nil }
+#       end
+#       email { 'sixplus2@test.com' }
+#       password { '12345678' }
+#       first_name { 'Fake' }
+#       last_name { 'User' }
+#       password_confirmation { '12345678' }
+#       sign_in_count { count.to_s }
+#       provider { 'shibboleth' }
+#       uid { 'sixplus2@test.com' }
+#     end
