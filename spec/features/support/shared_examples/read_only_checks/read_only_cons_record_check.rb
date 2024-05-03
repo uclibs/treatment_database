aa0a211bd46ed9e39c10366a8ffe045cbf42e9c3
Janell-Huyck <huyckjl@ucmail.uc.edu>
2024-05-03 15:31:58 -0400

    Check read-only cons records for access rights

---
+++ spec/features/support/shared_examples/read_only_checks/read_only_cons_record_check.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.shared_examples 'displays conservation records for read-only users correctly' do
+  let(:cons_record) { create(:conservation_record) }
+
+  before do
+    cons_record
+  end
+
+  it 'does not have buttons that it should not have' do
+    visit conservation_records_path
+    expect(page).not_to have_link('New Conservation Record')
+
+    within('table tbody') do
+      first('a').click
+    end
+
+    expect(page).not_to have_link('Edit Conservation Record')
+    expect(page).not_to have_link('Add In-House Repair')
+    expect(page).not_to have_link('Add External Repair')
+    expect(page).not_to have_link('Add Conservators and Technicians')
+  end
+
+  it 'has disabled conservation record buttons' do
+    visit conservation_records_path
+    within('table tbody') do
+      first('a').click
+    end
+
+    expect(page).to have_content('Cost and Return Information')
+    expect(page).to have_button('Save Cost and Return Information', disabled: true)
+    expect(page).to have_button('Save Treatment Report', disabled: true)
+    expect(page).to have_button('Save Cost and Return Information', disabled: true)
+  end
+
+  it 'has disabled input fields in the conservation record form' do
+    visit conservation_record_path(cons_record)
+    expect(page).to have_selector('h1', text: cons_record.title)
+    expect(page).to have_selector('form.disable_input')
+    all('form.disabled_input').each do |form|
+      within(form) do
+        # Check disabled state for all input, textarea, and select elements
+        %w[input textarea select].each do |element_type|
+          all(element_type).each do |element|
+            expect(element).to be_disabled
+          end
+        end
+      end
+    end
+  end
+end
