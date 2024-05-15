008d1f6582577bf068d4e8140d5f6faccae621dc
lisa3711 <lisa.haitz@uc.edu>
2024-05-14 21:30:46 -0400

    reviewed test list

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'factory_bot'
 require 'rspec/rails'
+
+#need below?
 require 'paper_trail/frameworks/rspec'
 require 'capistrano-spec'
 
