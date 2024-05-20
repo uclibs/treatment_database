2e812d7bf7205655b189fcbf5900b9bcc7235b61
Janell Huyck <Janell.Huyck@outlook.com>
2024-05-20 15:11:55 -0400

    Test update 5/20

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,24 +5,7 @@ FactoryBot.define do
     sequence(:display_name) { |n| "Name #{n}" }
     sequence(:email) { |n| "user#{n}_#{Time.now.to_i}@example.com" }
     password { 'notapassword' }
-    dont think we need : role { 'standard' }
+    role { 'standard' }
     account_active { true }
   end
 end
-
-# The following is from ucrate's user factory:
-#  factory :shibboleth_user, class: 'User' do
-#       transient do
-#         count { 1 }
-#         person_pid { nil }
-#       end
-# ***** might need to update these
-#       email { 'sixplus2@test.com' }
-#       password { '12345678' }
-#       first_name { 'Fake' }
-#       last_name { 'User' }
-#       password_confirmation { '12345678' }
-#       sign_in_count { count.to_s }
-#       provider { 'shibboleth' }
-#       uid { 'sixplus2@test.com' }
-#     end
