7a813afadde3169724889f00164811cfe5ebcede
Janell Huyck <janell.huyck@outlook.com>
2024-06-17 14:24:52 -0400

    519 - Add shared rake test context (#521)
    * Add new context for rake
    
    * Add shared context and include in rake task tests
    
    * Rubocop
    
    * Load custom rake tasks with shared context

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,12 +19,8 @@ require 'paper_trail/frameworks/rspec'
 # end with _spec.rb. You can configure this pattern with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
 #
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+# Auto-require all Ruby files in the spec/support directory.
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -35,6 +31,9 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  # Add shared context "rake" to rake task tests
+  config.include_context 'rake', type: :task
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = Rails.root.join('spec/fixtures').to_s
 
