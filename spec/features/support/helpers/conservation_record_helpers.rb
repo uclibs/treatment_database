c0cffc4c4c721d7810bc7442d5b25c50164c82f7
Janell Huyck <Janell.Huyck@outlook.com>
2024-07-02 13:13:04 -0400

    Fix broken test after rebase

--- spec/features/support/helpers/conservation_record_helpers.rb
+++ spec/features/support/helpers/conservation_record_helpers.rb
@@ -5,10 +5,10 @@ def add_in_house_repair(conservation_record)
   click_link(conservation_record.title, match: :prefer_exact)
   expect(page).to have_button('Add In-House Repairs')
   click_button('Add In-House Repairs')
-  select(user.display_name, from: 'in_house_repair_record_performed_by_user_id', match: :first)
-  select('Mend paper', from: 'in_house_repair_record_repair_type', match: :first)
-  fill_in 'in_house_repair_record_minutes_spent', with: '10'
-  fill_in 'in_house_repair_record_other_note', with: 'Other Note'
-  select('test', from: 'in_house_repair_record_staff_code_id', match: :first)
+  select(user.display_name, from: 'in_house_performed_by_user_id', match: :first)
+  select('Mend paper', from: 'in_house_repair_type', match: :first)
+  fill_in 'in_house_minutes_spent', with: '10'
+  fill_in 'in_house_other_note', with: 'Other Note'
+  select('test', from: 'in_house_staff_code_id', match: :first)
   click_button('Create In-House Repair Record')
 end
