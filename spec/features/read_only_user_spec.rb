103f9793dbebcf9afba83f8c64a3846077a661a0
Janell Huyck <Janell.Huyck@outlook.com>
2024-08-09 19:05:00 -0400

    Undo capybara setup moving since it's in a different PR

--- spec/features/read_only_user_spec.rb
+++ spec/features/read_only_user_spec.rb
@@ -2,6 +2,18 @@
 
 require 'rails_helper'
 
+require 'capybara'
+
+Capybara.register_driver :selenium_chrome_headless_sandboxless do |app|
+  browser_options = Selenium::WebDriver::Chrome::Options.new
+  browser_options.args << '--headless'
+  browser_options.args << '--disable-gpu'
+  browser_options.args << '--no-sandbox'
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+Capybara.default_driver = :rack_test # This is a faster driver
+Capybara.javascript_driver = :selenium_chrome_headless_sandboxless
+
 RSpec.describe 'Read Only User Tests', type: :feature, js: true do
   let(:user) { create(:user, role: 'read_only') }
   let(:conservation_record) { create(:conservation_record, title: 'Farewell to Arms', department: 'ARB Library') }
