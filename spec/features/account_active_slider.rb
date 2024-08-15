34751ea64d4f9d9bdbd45764ecf2a2239080794b
Janell Huyck <Janell.Huyck@outlook.com>
2024-08-15 15:28:35 -0400

    Clean up account_active_slider test

--- spec/features/account_active_slider.rb
+++ spec/features/account_active_slider.rb
@@ -5,8 +5,6 @@ require 'rails_helper'
 RSpec.feature 'Account Active Slider', type: :feature, js: true do
   let(:admin_user) { create(:user, role: 'admin') }
   let(:standard_user) { create(:user, role: 'standard', account_active: false) }
-  let(:conservation_record) { create(:conservation_record, title: 'Farewell to Arms', department: 2) }
-  let(:vocabulary) { create(:controlled_vocabulary) }
 
   scenario 'user toggles the account active slider and label changes' do
     log_in_user(admin_user)
