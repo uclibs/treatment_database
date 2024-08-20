45a0058b1c81c70b4387fb601ff6b41146786f2e
Janell Huyck <Janell.Huyck@outlook.com>
2024-08-20 09:25:56 -0400

    WIP

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,5 +79,4 @@ RSpec.configure do |config|
   config.fixture_path = Rails.root.join('spec/fixtures').to_s
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-
 end
