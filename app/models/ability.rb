48050c150a8f3a23aad884e043b08cb0d4d020e1
Janell-Huyck <huyckjl@ucmail.uc.edu>
2024-08-21 14:29:27 -0400

    Update Ability model to match new layout

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,45 +5,31 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    define_alias_actions
-    assign_permissions_for(user)
-  end
 
-  private
-
-  def define_alias_actions
     alias_action :create, :read, :update, :destroy, to: :crud
     alias_action :treatment_report, :abbreviated_treatment_report, :conservation_worksheet, to: :view_pdfs
-  end
 
-  def assign_permissions_for(user)
     case user.role
     when 'admin'
-      admin_permissions
+      can :manage, :all
+      cannot :destroy, StaffCode
+      cannot :destroy, User
     when 'standard'
-      standard_permissions(user)
+      can :view_pdfs, ConservationRecord
+      can :crud, [ConservationRecord, ExternalRepairRecord, InHouseRepairRecord, ConTechRecord, CostReturnReport]
+      can :index, ConservationRecord
+      # Allow standard users to read and update their own user account, but not index other users
+      can %i[read update], User, id: user.id
+      cannot :index, User
+    when 'read_only'
+      can :view_pdfs, ConservationRecord
+      can :read, ConservationRecord
+      can :index, ConservationRecord
+      # Allow read_only users to read and update their own user account, but not index other users
+      can %i[read update], User, id: user.id
+      cannot :index, User
     else
-      read_only_permissions(user)
+      cannot :manage, :all
     end
-  end
-
-  def admin_permissions
-    can :manage, :all
-    cannot :destroy, StaffCode
-  end
-
-  def standard_permissions(user)
-    can :view_pdfs, ConservationRecord
-    can :crud, [ConservationRecord, ExternalRepairRecord, InHouseRepairRecord, ConTechRecord, CostReturnReport]
-    can %i[read update], User, id: user.id
-    cannot :index, User
-  end
-
-  def read_only_permissions(user)
-    can :view_pdfs, ConservationRecord
-    can :read, ConservationRecord
-    can :index, ConservationRecord
-    can %i[read update], User, id: user.id
-    cannot :index, User
   end
 end
