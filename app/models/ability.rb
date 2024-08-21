54c4faa2213e556f28d761ce27f8e800d5203eef
Janell-Huyck <huyckjl@ucmail.uc.edu>
2024-08-21 12:46:14 -0400

    LIBTREATDB-78 Remove application logic from navigation html

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,32 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    # Define abilities for the passed in user here. For example:
+    #
+    #   user ||= User.new # guest user (not logged in)
+    #   if user.admin?
+    #     can :manage, :all
+    #   else
+    #     can :read, :all
+    #   end
+    #
+    # The first argument to `can` is the action you are giving the user
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on.
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, :published => true
+    #
+    # See the wiki for details:
+    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
     user ||= User.new # guest user (not logged in)
 
     alias_action :create, :read, :update, :destroy, to: :crud
@@ -13,17 +39,13 @@ class Ability
     when 'admin'
       can :manage, :all
       cannot :destroy, StaffCode
-      cannot :destroy, User
     when 'standard'
       can :view_pdfs, ConservationRecord
       can :crud, [ConservationRecord, ExternalRepairRecord, InHouseRepairRecord, ConTechRecord, CostReturnReport]
-      can :index, ConservationRecord
     when 'read_only'
       can :view_pdfs, ConservationRecord
       can :read, ConservationRecord
       can :index, ConservationRecord
-    else
-      cannot :manage, :all
     end
   end
 end
