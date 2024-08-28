b6b7a9273e226832ae880c002e99ed8e860d8bef
Janell-Huyck <huyckjl@ucmail.uc.edu>
2024-08-28 16:17:44 -0400

    Remove possible user creation by non-admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,19 +17,16 @@ class UsersController < ApplicationController
 
   def create_user
     @user = User.new(user_params)
+
     if @user.save
-      if current_user.present? && current_user.role == 'admin'
-        redirect_to users_path
-      else
-        sign_in(@user)
-        redirect_to after_sign_in_path_for(@user)
-      end
-      flash.now[:notice] = 'Successfully created User.'
+      flash[:notice] = 'Successfully created User.'
+      redirect_to users_path
     else
       flash.now[:notice] = @user.errors.full_messages.first
       redirect_back fallback_location: root_path
     end
   end
+
 
   def update
     @user = User.find(params[:id])
