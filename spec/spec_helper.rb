24719c6dec7f545a7d2a4c6195d3657e255ee921
Janell-Huyck <huyckjl@ucmail.uc.edu>
2024-08-28 09:25:34 -0400

    Pull out simplecov setup into its own file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,9 @@
 # frozen_string_literal: true
 
-require 'simplecov'
-require 'simplecov-lcov'
-require 'coveralls'
-
-SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-
-SimpleCov.start 'rails' do
-  command_name "RSpec_#{ENV.fetch('CIRCLE_NODE_INDEX', nil)}" # Unique command name per node
-  coverage_dir "coverage/#{ENV.fetch('CIRCLE_NODE_INDEX', nil)}" # Store results in unique directories per node
-  add_filter '/spec/'
-  add_filter '/lib/tasks/'
-  add_filter '/lib/capistrano/'
-end
-
-SimpleCov.at_exit do
-  SimpleCov.result.format! if ENV['CIRCLE_NODE_INDEX'] # Only format individual results
-end
-
 require 'byebug'
-
 require 'devise'
+
+require_relative '../simplecov_setup'
 
 def sign_in(user)
   post user_session_path \
