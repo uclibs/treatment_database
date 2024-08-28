635736e6f05cf5358012256d47a0ae3ab43a22c1
Janell-Huyck <huyckjl@ucmail.uc.edu>
2024-08-28 15:55:52 -0400

    Add more tests to users_controller_spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,10 +32,17 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update!(user_params)
-    redirect_to users_path
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash[:notice] = 'User was successfully updated.'
+      redirect_to users_path
+    else
+      flash.now[:alert] = @user.errors.full_messages.first
+      render :edit
+    end
   end
+
 
   private
 
