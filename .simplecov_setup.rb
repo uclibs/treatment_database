c18fe6b3ecb059169e5f69bb19646ad5070b68d9
Janell-Huyck <huyckjl@ucmail.uc.edu>
2024-08-28 09:47:17 -0400

    Remove merge_coverage script

--- .simplecov_setup.rb
+++ .simplecov_setup.rb
@@ -4,28 +4,19 @@ require 'simplecov'
 require 'simplecov-lcov'
 require 'coveralls'
 
-# Configure LCOV Formatter for Coveralls
-SimpleCov::Formatter::LcovFormatter.config do |config|
-  config.report_with_single_file = true
-  config.output_directory = 'coverage/lcov' # Ensure the LCOV report is stored here
-  config.lcov_file_name = 'lcov.info'       # Set the file name to 'lcov.info'
-end
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 
 SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
-                                                                  SimpleCov::Formatter::LcovFormatter,     # Generates LCOV file for Coveralls
-                                                                  SimpleCov::Formatter::HTMLFormatter      # Generates the HTML coverage report
+                                                                  SimpleCov::Formatter::LcovFormatter,
+                                                                  SimpleCov::Formatter::HTMLFormatter,
+                                                                  Coveralls::SimpleCov::Formatter
                                                                 ])
 
-# Start SimpleCov for Rails
+# Enable merging of results across parallel runs
 SimpleCov.start 'rails' do
-  command_name "RSpec_#{ENV.fetch('CIRCLE_NODE_INDEX', '0')}" # Unique command name per node
-  coverage_dir "coverage/#{ENV.fetch('CIRCLE_NODE_INDEX', '0')}" # Store results in unique directories per node
+  enable_coverage :branch
+  merge_timeout 3600 # Ensure there's enough time to merge results across jobs
   add_filter '/spec/'
   add_filter '/lib/tasks/'
   add_filter '/lib/capistrano/'
-end
-
-# Only format individual results on parallel nodes
-SimpleCov.at_exit do
-  SimpleCov.result.format! if ENV['CIRCLE_NODE_INDEX'] # Only format individual results
 end
