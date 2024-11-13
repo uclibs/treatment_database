33598634d93c74b867f8bb5f75dcb5ce65d0716b
Janell-Huyck <huyckjl@ucmail.uc.edu>
2024-11-13 10:07:31 -0500

    WIP - add logging headers and request.env

--- app/controllers/concerns/authentication_concern.rb
+++ app/controllers/concerns/authentication_concern.rb
@@ -15,6 +15,24 @@ module AuthenticationConcern
       process_shibboleth_login
     else
       Rails.logger.error 'Shibboleth attributes not present. Cannot authenticate user.'
+
+      Rails.logger.error 'request.env attributes are:'
+      if request.env[attr].is_a?(Enumerable)
+        request.env[attr].each do |attribute|
+          Rails.logger.error attribute
+        end
+      else
+        Rails.logger.error "No attributes found in request.env[#{attr}] or it is not iterable."
+      end
+
+      Rails.logger.error 'request.header items are:'
+      if request.headers.is_a?(Enumerable)
+        request.headers.each do |header_item|
+          Rails.logger.error header_item
+        end
+      else
+        Rails.logger.error "No attributes found in request.headers or it is not iterable."
+      end
       redirect_to root_path, alert: 'Authentication failed: Shibboleth attributes not present.'
     end
   end
